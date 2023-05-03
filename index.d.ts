014ed9813270eb57fb99a02ca496ce27f4aa367d
Moscarde <theizera@gmail.com>
2023-05-03 00:56:56 -0300

    git rm --cached

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+interface Person {
+    name: string;
+    email: string;
+    age: number;
+    birthDate: string;
+}
+export declare function randomUser(genre?: string, minAge?: number, maxAge?: number): Person;
+export {};
---
+++ src/names.d.ts
@@ -0,0 +1,19 @@
+interface Names {
+    brazilian: {
+        male: Array<string>;
+        female: Array<string>;
+        lastName: Array<string>;
+    };
+    northAmerican?: {
+        male: Array<string>;
+        female: Array<string>;
+        lastName: Array<string>;
+    };
+    latin?: {
+        male: Array<string>;
+        female: Array<string>;
+        lastName: Array<string>;
+    };
+}
+export declare const names: Names;
+export {};

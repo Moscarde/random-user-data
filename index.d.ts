c588074d876ad53073974a580eda8bb9d00f7b96
Moscarde <theizera@gmail.com>
2023-05-03 00:56:22 -0300

    git rm --cached

--- index.d.ts
+++
@@ -1,8 +0,0 @@
-interface Person {
-    name: string;
-    email: string;
-    age: number;
-    birthDate: string;
-}
-export declare function randomUser(genre?: string, minAge?: number, maxAge?: number): Person;
-export {};
--- src/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.randomNumber = void 0;
-/** Returns a random number from 0 to 100, where 100 is the length of the names arrays. */
-function randomNumber() {
-    return Math.floor(Math.random() * 100);
-}
-exports.randomNumber = randomNumber;
